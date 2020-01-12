53b93e9992dd40cfa0af31ec4b1db7a8d47424a4
Lauren Yu <laurenkyu@gmail.com>
2020-01-12 21:20:50 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,31 +4,31 @@
 #
 # The tests will guide your construction.
 #
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+PORTION_1 = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class"
+    }
+  }
+}
+
+PORTION_2 = {
+  label: "Order"
+}
+
+PORTION_3 = {
+  label: "Family",
+  sub_category: {
+    label: "Genus",
+  }
+}
+
+PORTION_4 = {
+  label: "Species",
+  sub_category: nil
+}
 
 def naming_system
   # Remember:
@@ -42,4 +42,13 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
 
+  naming_system = PORTION_1
+  naming_system[:sub_category][:sub_category][:sub_category] = PORTION_2
+  naming_system[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
+  naming_system[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
+  naming_system
 end
+
+#naming_system
+
+
